0dc27bf26569726696877f004613be1b76398677
Liam Mulvaney <liam.m.m.mulvaney@gmail.com>
2019-08-26 15:59:16 +0200

    IBusinessLogic & Repository
    IBusiness Logic Interface first commit.
    
    Added a new filter for the List method in the IRepository class and its implementation Class

---
+++ ProjectX.WebAPI/ProjectX.BusinessLogic/Interfaces/IEmployeeBL.cs
@@ -0,0 +1,18 @@
+﻿using ProjectX.Models.Models;
+using ProjectX.UnitOfWork.UOW.Interface;
+using System;
+using System.Collections.Generic;
+
+namespace ProjectX.BusinessLogic.Interfaces
+{
+    public interface IEmployeeBL
+    {
+        IUOW<Employee> UnitOfWork { get; set; }
+        IEnumerable<Employee> GetAllEmployeesAsync(Guid PartitionId);
+        IEnumerable<Employee> GetAllEmployeesAsync(Func<Employee, bool> condition);
+        void CreateEmployeeAsync(Employee EmployeeToCreate);
+        void UpdateEmployeeAsync(Employee EmployeeToUpdate);
+        void DeleteEmployeeAsync(Employee EmployeeToDelete);
+
+    }
+}
